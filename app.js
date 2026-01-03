bdce0074a054585a5b1f25cd55f1828d70a59727
nuwan-dev <lakesoft.tech@gmail.com>
2026-01-03 16:41:15 +0530

    mobile menu initial

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-// nav btn
-function rotateNav() {}
-
 const mbMenuBtn = document.querySelector(".mb-menu-btn");
+const mbNavMenu = document.querySelector(".invoicee-mb-nav-container");
 mbMenuBtn.addEventListener("click", () => {
   mbMenuBtn.classList.toggle("mb-menu-btn-active");
+  mbNavMenu.classList.toggle("mb-nav-active");
 });
