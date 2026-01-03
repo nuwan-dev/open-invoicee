96d563bc13f7b2aebd6436e479ff1bbf03b907a4
nuwan-dev <lakesoft.tech@gmail.com>
2026-01-03 15:31:11 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// nav btn
+function rotateNav() {}
+
+const mbMenuBtn = document.querySelector(".mb-menu-btn");
+mbMenuBtn.addEventListener("click", () => {
+  mbMenuBtn.classList.toggle("mb-menu-btn-active");
+});
